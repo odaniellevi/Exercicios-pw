b55b9b565f9f7e414d2ac53e2bc3d222a6a9bc35
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2024-08-30 22:24:43 +0000

    somatorio 2 finalizado

--- Somatório (ATV)/somatorio.js
+++ Somatório (ATV)/somatorio.js
@@ -1,7 +1,11 @@
 const btAdic = document.getElementById("btAdic");
+const btLimp = document.getElementById("btLimp");
 const inputNum = document.getElementById("inputNum");
 const inputSoma = document.getElementById("inputSoma");
+const inputMedia = document.getElementById("inputMedia");
+const inputQtd = document.getElementById("inputQtd");
 let somatorio = 0;
+let qtd = 0;
 
 btAdic.onclick = () => {
     let num = parseInt(inputNum.value);
@@ -9,8 +13,21 @@ btAdic.onclick = () => {
         alert("Digite um número inteiro!");
         return;
     }
+
+    ++qtd;
     somatorio += num;
-    inputSoma.value = somatorio;   
+    const media = somatorio / qtd;
+    inputSoma.value = somatorio;
+    inputMedia.value = media;
+    inputQtd.value = qtd;   
     inputNum.value = "";
     inputNum.focus();
-}
+};
+
+btLimp.onclick = () => {
+    inputSoma.value = 0;
+    inputMedia.value = 0;
+    inputQtd.value = 0;
+    inputNum.value = "";
+    inputNum.focus();
+};
