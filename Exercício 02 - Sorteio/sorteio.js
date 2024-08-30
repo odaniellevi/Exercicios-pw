502035bef38806e9e6be224ca6b52154ba51cb99
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2024-08-30 23:03:08 +0000

    commit de controle

---
+++ Exercício 02 - Sorteio/sorteio.js
@@ -0,0 +1,7 @@
+const btSortear = document.getElementById("btSortear");
+const inputMenor = document.getElementById("inputMenor");
+const inputMaior = document.getElementById("inputMaior");
+let sortear = 0;
+
+document.getElementById("demo").innerHTML =
+Math.floor(Math.random() * 100) + 1;
