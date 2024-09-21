0dd733376f56fe627b4735e246c237c2b7c45baa
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2024-09-21 13:33:05 +0000

    commit de controle 1

---
+++ Exercício 02 - Sorteio/script.js
@@ -0,0 +1,13 @@
+document.getElementById("btSortear").addEventListener("click", function() {
+    let menor = parseInt(document.getElementById("inputMenor").value);
+    let maior = parseInt(document.getElementById("inputMaior").value);
+
+    let (isNaN(menor) || isNaN(maior)) {
+        alert("Por favor, insiravalores numéricos.");
+        return;
+    }
+
+    if (menor < 1 || maior > 100 || menor >= maior) {
+        alert
+    }
+}
