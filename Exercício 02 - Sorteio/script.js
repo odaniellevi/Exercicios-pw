96f739c825b921e5e6984d6fa4e895f2d01ada09
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2024-09-21 13:59:34 +0000

    atividade sorteio modificada  e finalizada

--- Exercício 02 - Sorteio/script.js
+++ Exercício 02 - Sorteio/script.js
@@ -2,12 +2,17 @@ document.getElementById("btSortear").addEventListener("click", function() {
     let menor = parseInt(document.getElementById("inputMenor").value);
     let maior = parseInt(document.getElementById("inputMaior").value);
 
-    let (isNaN(menor) || isNaN(maior)) {
-        alert("Por favor, insiravalores numéricos.");
+    if (isNaN(menor) || isNaN(maior)) {
+        alert("Por favor, insira valores numéricos.");
         return;
     }
 
     if (menor < 1 || maior > 100 || menor >= maior) {
-        alert
+        alert("Os valores devem estar entre 1 e 100, e  o menor deve ser menor que o maior.");
+        return;
     }
-}
+
+    let sorteado = Math.floor(Math.random() * (maior - menor + 1)) + menor;
+
+    document.getElementById("resultado").value = sorteado;
+});
