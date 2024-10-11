08596416aaae13993d99834d7bde65db1f4f6aff
Daniel Levi <163361305+odaniellevi@users.noreply.github.com>
2024-10-11 22:53:17 +0000

    iniciando crud

---
+++ CRUD Despesas/script.js
@@ -0,0 +1,5 @@
+const baseUrl = "https://parseapi.back4app.com/classes/Despesas";
+const headers = { 
+    "X-Parse-Application-Id": "AprcXNQtxwGIADPbOdFzEDFf1KN22MmZ3C1kLN3x" ,
+    "X-Parse-REST-API-Key": "IUxSg4R9aja3wj6EStkDdIBzSjU555q9m0gmey9F" ,
+    };
